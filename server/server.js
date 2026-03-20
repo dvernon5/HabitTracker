3a56057af02d37f61f88b1151e0ac3a58f9ebd39
Donovan Vernon <d.vernon5@gmail.com>
2026-03-20 09:40:20 -0700

    Added server listener with port number

--- server/server.js
+++ server/server.js
@@ -8,8 +8,14 @@ const checkinRouter = require("./routes/checkinRouter");
 // Make prisma accessible across all route files
 app.locals.prisma = prisma;
 
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/habits", habitRouter);
 app.use("/checkins", checkinRouter);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Now listening on http://localhost:${ PORT }`);
+});
