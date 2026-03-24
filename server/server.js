1a6fd5efb2439fd4c1beb360652743270ce447ce
Donovan Vernon <d.vernon5@gmail.com>
2026-03-23 21:47:47 -0700

    Added a middleware to serve static pages

--- server/server.js
+++ server/server.js
@@ -1,21 +1,25 @@
+require('dotenv').config();
 const express = require("express");
-const app = express();
 const { PrismaClient } = require("@prisma/client");
+const { auth } = require('express-openid-connect');
+
+const app = express();
 const prisma = new PrismaClient();
-const habitRouter = require("./routes/habitRouter");
-const checkinRouter = require("./routes/checkinRouter");
 
 // Make prisma accessible across all route files
 app.locals.prisma = prisma;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("client/public"));
 
+const habitRouter = require("./routes/habitRouter");
+const checkinRouter = require("./routes/checkinRouter");
 app.use("/habits", habitRouter);
 app.use("/checkins", checkinRouter);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Now listening on http://localhost:${ PORT }`);
 });
