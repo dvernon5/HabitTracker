e3d3367226fe25557171c5a116c1599e0a38728b
Donovan Vernon <d.vernon5@gmail.com>
2026-03-23 22:57:23 -0700

    Created callback that handles hidden form submission authenication

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -21,7 +21,7 @@ authRouter.get("/login", async (req, res) => {
     }
 });
 
-// Verifying token via callback. Create session if token is found.
+// Verifying token in the URL via callback. Create session if token is found.
 authRouter.get("/callback", async (req, res) => {
     try {
         await res.oidc.callback({
@@ -30,4 +30,15 @@ authRouter.get("/callback", async (req, res) => {
     } catch (err) {
         res.status(500).json({ message: "Authentication failed", error: err.message });
     }
-})
+});
+
+// Verifying token in a hidden form via callback. Create session if token is found.
+authRouter.post("/callback", express.urlencoded({ extended: false }), async (req, res) => {
+    try {
+        await res.oidc.callback({
+            redirectUri: 'http://localhost:3000/callback',
+        });
+    } catch(err) {
+        res.status(500).json({ message: "Authentication failed", error: err.message });
+    }
+});
